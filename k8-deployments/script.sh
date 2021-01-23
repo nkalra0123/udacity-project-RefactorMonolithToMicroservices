7111111f1364bbf2fffeeeaf3a1a114539907c2c
nkalra0123 <nkalra0123@gmail.com>
2021-01-23 15:26:37 +0530

    Adding script for deployment

--- k8-deployments/script.sh
+++ k8-deployments/script.sh
@@ -1,1 +1,13 @@
 kubectl apply -f secret_local.yml
+
+kubectl apply -f udagram-api-feed-deployment.yaml
+kubectl apply -f udagram-api-feed-service.yaml
+
+kubectl apply -f udagram-api-users-deployment.yaml
+kubectl apply -f udagram-api-users-service.yaml
+
+kubectl apply -f udagram-frontend-deployment.yaml
+kubectl apply -f udagram-frontend-service.yaml
+
+kubectl apply -f reverse-proxy-deployment.yaml
+kubectl apply -f reverse-proxy-service.yaml
